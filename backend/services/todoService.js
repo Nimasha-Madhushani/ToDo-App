bd8e8e61c3aa0077145763216db371541e42aeb4
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2024-01-13 19:23:13 +0530

    complete get request

--- backend/services/todoService.js
+++ backend/services/todoService.js
@@ -1,15 +1,25 @@
-const TodoModel = require('../models/model');
+const TodoModel = require("../models/model");
 
 const createTodo = async (task) => {
   try {
     const result = await TodoModel.create({ task: task });
     return result;
   } catch (err) {
-    console.error('Error saving to database:', err);
+    console.error("Error saving to database:", err);
     throw err;
+  }
+};
+
+const getTodo = async () => {
+  try {
+    const result = await TodoModel.find();
+    return result;
+  } catch (error) {
+    console.error("Error retrieving from database:", err);
   }
 };
 
 module.exports = {
   createTodo,
+  getTodo,
 };
